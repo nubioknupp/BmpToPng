0f6e884833c847dec56abb77f8d9d3ea613b39d3
Núbio Knupp <nubioknupp@gmail.com>
2015-10-04 09:30:22 -0300

    Bmp to Png in C#
    Creates PNG files with transparent backgrounds of a bmp image in C#.

---
+++ BmpToPng/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace BmpToPng
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var bmp = new Bitmap(@"D:\Temp\logo.bmp"))
+            {
+                bmp.MakeTransparent();
+                bmp.Save(@"D:\Temp\logo.png", ImageFormat.Png);
+            }
+        }
+    }
+}
